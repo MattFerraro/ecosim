102e6082140a38131ac2128eee0709ef408949eb
Chrisantha Perera <chrisantha@alum.mit.edu>
2020-09-05 19:48:01 -0700

    Omnivores onthemove

--- Assets/Omnivore.cs
+++ Assets/Omnivore.cs
@@ -8,32 +8,44 @@ public class Omnivore : MonoBehaviour
     // genome is set at birth and immutable
     public Dictionary<string,Gene> genome;
     // non-heritable state:
-    float energy = 30.0f; 
+    float energy = 30.0f;
+    Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        target = rb.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // navigate to a target
+        if (Vector3.Distance(rb.position,target)<1.0){
+            // do stuff with target
+            // then set new target
+            target = new Vector3(
+                Random.value * 100 - 50, 1, Random.value * 100 - 50
+            );
+        }
+
+
         // find that nearest tree
         // GameObject target = closestVisible("Tree");
         // GameObject danger = closestVisible("Fox");
         // GameObject suitor = closestVisible("Bunny");
-        // if (target != null){
-        //     // Debug.Log("Found a tree!");
-        //     // move to it
-        //     if (Vector3.Distance(rb.position,target.transform.position) > 1.1){
-        //         Vector3 move = Vector3.MoveTowards(rb.position,target.transform.position,0.07f);
-        //         rb.MovePosition(move);
-        //     } else {
+        if (target != null){
+            // Debug.Log("Found a tree!");
+            // move to it
+            if (Vector3.Distance(rb.position,target) >.1){
+                Vector3 move = Vector3.MoveTowards(rb.position,target,0.07f);
+                rb.MovePosition(move);
+            }// else {
         //         //GameObject.Destroy(target);
         //         rb.AddForce(new Vector3(0.0f,0.0f,0.0f),ForceMode.VelocityChange);
         //     }
-        // }
+        }
     }
 // SetGenome will set this animal's genome, then set properties based on it
     public void SetGenome(Dictionary<string,Gene> genome){
